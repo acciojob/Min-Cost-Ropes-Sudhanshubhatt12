9f2d1214bd639691979ed4f5d55350947fb8881b
Sudhanshu Bhatt <101445771+Sudhanshubhatt12@users.noreply.github.com>
2025-01-02 15:10:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,85 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    extractMin() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return min;
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index] >= this.heap[parentIndex]) break;
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            index = parentIndex;
+        }
+    }
+
+    bubbleDown() {
+        let index = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild < element) swap = leftChildIndex;
+            }
+
+            if (rightChildIndex < length) {
+                rightChild = this.heap[rightChildIndex];
+                if (
+                    (swap === null && rightChild < element) ||
+                    (swap !== null && rightChild < leftChild)
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) break;
+            [this.heap[index], this.heap[swap]] = [this.heap[swap], this.heap[index]];
+            index = swap;
+        }
+    }
+
+    size() {
+        return this.heap.length;
+    }
 }
 
-module.exports=mincost;
+function mincost(arr) {
+    const minHeap = new MinHeap();
+    let totalCost = 0;
+
+    // Insert all rope lengths into the min-heap
+    arr.forEach(length => minHeap.insert(length));
+
+    // Combine ropes until only one rope remains
+    while (minHeap.size() > 1) {
+        const first = minHeap.extractMin();
+        const second = minHeap.extractMin();
+        const cost = first + second;
+        totalCost += cost;
+        minHeap.insert(cost);
+    }
+
+    return totalCost;
+}
+
+module.exports = mincost;
